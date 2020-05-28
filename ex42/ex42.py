56e7aeb54262ce470188cd4f776d93f2aec070a5
FoctoDev <foctodev@gmail.com>
2020-05-28 14:28:14 +0500

    Task addition

--- ex42/ex42.py
+++ ex42/ex42.py
@@ -23,14 +23,54 @@ class Person(object):
         ## переменная name экземпляра класса Person
         self.name = name
 
-        ## Person - композиция животного некоторого вида
+        ## Person - композиция животного некоторого вида, т.е класс Person содержит объект pet другого класса (животные)
         self.pet = None
 
-##
+## Employee населдует Person
 class Employee(Person):
 
     def __init__(self, name, salary):
         ## вызывает метод инициализации из родительского класса, чтобы дополнить его
         super(Employee, self).__init__(name)
-        ## переменная name экземпляра класса Employee
+        ## переменная salary экземпляра класса Employee
         self.salary = salary
+
+## Fish населдует object
+class Fish(object):
+    pass
+
+## Salmon населдует Fish
+class Salmon(Fish):
+    pass
+
+## Halibut населдует Fish
+class Halibut(Fish):
+    pass
+
+
+## barbos населдует Dog
+barbos = Dog("Барбос")
+
+## barsik населдует Cat
+barsik = Cat("Барсик")
+
+## mary населдует Person
+mary = Person("Машка")
+
+## mary.pet населдует barsik, который наследует Cat
+mary.pet = barsik
+
+## filka населдует Employee
+filka = Employee("Филька", 120000)
+
+## filka.pet населдует barbos, который наследует Dog
+filka.pet = barbos
+
+## flipper населдует Fish
+flipper = Fish()
+
+## crouse населдует Salmon
+crouse = Salmon()
+
+## harry населдует Halibut
+harry = Halibut()
