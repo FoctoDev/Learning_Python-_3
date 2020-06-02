5dd0afd818a854911aeb74b16aba757715d092d2
FoctoDev <foctodev@gmail.com>
2020-06-02 17:02:28 +0500

    Explicit Override

---
+++ ex44/ex44b.py
@@ -0,0 +1,17 @@
+class Parent(object):
+
+    def override(self):
+        print("РОДИТЕЛЬ override()")
+
+
+class Child(Parent):
+
+    def override(self):
+        print("ПОТОМОК override()")
+
+
+dad = Parent()
+son = Child()
+
+dad.override()
+son.override()
