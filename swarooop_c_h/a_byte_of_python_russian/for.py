f52693395063f5acc6f2453a84367593412d17f6
FoctoDev <foctodev@gmail.com>
2020-06-11 17:25:48 +0500

    Operator 'for'

---
+++ swarooop_c_h/a_byte_of_python_russian/for.py
@@ -0,0 +1,11 @@
+# range -- диапазон, интервал, т.е for i in range(1, 5) эквивалентно for i in [1, 2, 3 ,4]
+for i in range(1, 5):
+    print(i)
+else:
+    print('Цикл закончен')
+
+# Третье число range -- это шаг
+for i in range(1, 5, 2):
+    print(i)
+else:
+    print('Цикл закончен')
